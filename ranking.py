a0aa39bb88ba0886b150c3a821f660286dd3631e
Abhishek Sontakke <asontakke2@gmail.com>
2020-05-14 17:59:28 -0400

    Implemented CV and predictions

--- ranking.py
+++ ranking.py
@@ -1,25 +1,45 @@
+# TODO: Clean this up
 import pandas as pd
-from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.metrics import roc_auc_score, roc_curve, classification_report
+import matplotlib.pyplot as plt
 from sklearn import metrics
-import seaborn as sn
-
+import numpy as np
 
 df = pd.read_csv("sample_data.csv")
-X = df.loc[:, df.columns != 'Outcome'] # Features
-y = df.Outcome # Target variable
 
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=1)
+# Designate all columns that are not `Outcome` as features and `Outcome` as target
+X = df.loc[:, df.columns != 'Outcome']
+y = df.Outcome
 
-# instantiate the model (using the default parameters)
-logreg = LogisticRegression(fit_intercept=False)
+X_train,X_validate,y_train,y_validate=train_test_split(X,y,test_size=0.20,random_state=0)
 
-# fit the model with data
-logreg.fit(X_train,y_train)
+max_iter=[100,110,120,130,140]
+C = [0.01, 0.1, 1, 10, 100, 1000]
+penalty = ['l1','l2']
+fit_intercept=[False]
+param_grid = dict(max_iter=max_iter,C=C, penalty=penalty, fit_intercept=fit_intercept)
+
+lr = LogisticRegression()
+grid = GridSearchCV(estimator=lr, param_grid=param_grid, cv = 3, n_jobs=-1, scoring='accuracy')
+best_model = grid.fit(X_train, y_train)
+
+print("Best score: {0} using {1}".format(round(best_model.best_score_,2), best_model.best_params_))
+
+y_true, y_pred = y_validate, best_model.predict(X_validate)
+print(classification_report(y_true, y_pred))
+
+final_model = LogisticRegression(penalty=best_model.best_params_['penalty'],
+                                 C=best_model.best_params_['C'],
+                                 fit_intercept=best_model.best_params_['fit_intercept'],
+                                 max_iter=best_model.best_params_['max_iter'])
+
+final_model = final_model.fit(X, y)
 
 features = list(df.columns)
 features.remove("Outcome")
-[coef] = logreg.coef_.tolist()
+[coef] = final_model.coef_.tolist()
 
 rounded_coef = []
 for number in coef:
@@ -29,6 +49,21 @@ for number in coef:
 x = zip(rounded_coef, features)
 print(sorted(list(x)))
 
-# predict
-y_pred = logreg.predict(X_test)
-print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test, y_test)))
+result = final_model.predict_proba([[0,0,1,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]])
+
+print("The probability that Marc beats Rushi is {0}%".format(round(result[0][0]*100,2)))
+
+result = final_model.predict_proba([[-1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]])
+
+print("The probability that Shaq beats Gray is {0}%".format(round(result[0][0]*100,2)))
+
+result = final_model.predict_proba([[-1,-1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1]])
+
+print("The probability that Shaq and Gray beat Rushi is {0}%".format(round(result[0][0]*100,2)))
+result = final_model.predict_proba([[0,0,0,-1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0]])
+
+print("The probability that Marc beats Sam is {0}%".format(round(result[0][0]*100,2)))
+
+result = final_model.predict_proba([[0,0,1,-1,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,-1]])
+
+print("The probability that Marc and Sam beat Rushi is {0}%".format(round(result[0][0]*100,2)))
