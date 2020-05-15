4651bf398c253c8133a3d4da0aadd527e6350c7d
Abhishek Sontakke <asontakke2@gmail.com>
2020-05-15 14:26:17 -0400

    Simplified some of the parameter tuning but still don't know how to best pick

--- ranking.py
+++ ranking.py
@@ -65,12 +65,11 @@ y = invert_and_combine(y)
 # y_train = invert_and_combine(y_train)
 # y_validate = invert_and_combine(y_validate)
 
-max_iter=[100,110,120,130,140]
-C = [0.01, 0.1, 1, 10, 100, 1000]
-penalty = ['l1','l2']
-solver = ['newton-cg', 'lbfgs', 'sag', 'saga', 'liblinear']
+C = [0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0]
+penalty = ['l2']
+solver = ['liblinear']
 fit_intercept=[False]
-param_grid = dict(max_iter=max_iter,C=C, penalty=penalty, fit_intercept=fit_intercept, solver=solver)
+param_grid = dict(C=C, penalty=penalty, fit_intercept=fit_intercept, solver=solver)
 
 lr = LogisticRegression()
 grid = GridSearchCV(estimator=lr, param_grid=param_grid, cv = 5, n_jobs=-1, scoring='accuracy')
@@ -78,13 +77,18 @@ best_model = grid.fit(X, y)
 
 print("Best score: {0} using {1}".format(round(best_model.best_score_,2), best_model.best_params_))
 
+means = best_model.cv_results_['mean_test_score']
+stds = best_model.cv_results_['std_test_score']
+for mean, std, params in zip(means, stds, best_model.cv_results_['params']):
+    print("%0.3f (+/-%0.03f) for %r"
+          % (mean, std * 2, params))
+
 # y_true, y_pred = y_validate, best_model.predict(X_validate)
 # print(classification_report(y_true, y_pred))
 
 final_model = LogisticRegression(penalty=best_model.best_params_['penalty'],
                                  C=best_model.best_params_['C'],
                                  fit_intercept=best_model.best_params_['fit_intercept'],
-                                 max_iter=best_model.best_params_['max_iter'],
                                  solver=best_model.best_params_['solver'])
 
 final_model = final_model.fit(X, y)
