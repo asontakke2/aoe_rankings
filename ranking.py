b1d8cae6e60c42e5bc3b034dd9182c64356a9375
Abhishek Sontakke <asontakke2@gmail.com>
2020-05-19 13:31:28 -0400

    broke out data transformations into utility

--- ranking.py
+++ ranking.py
@@ -10,56 +10,12 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV, train_test_split
 import numpy as np
 from models.utilities.predictions import make_predictions
+from models.utilities.transformations import invert_and_combine
 
 df = pd.read_csv("data/sample_data.csv")
 
 X = df.loc[:, df.columns != 'Outcome']
 y = df.Outcome
-
-
-def invert_dataframe(original_dataframe):
-    """Inverts the dataframe by simply multiplying all values by -1.
-
-    Args:
-        original_dataframe (df): The dataframe to be inverted.
-
-    Returns:
-        inverted_dataframe (df): The inverted dataframe.
-
-    """
-    inverted_dataframe = original_dataframe.multiply(-1)
-    return inverted_dataframe
-
-
-def combine_dataframe(first_dataframe, second_dataframe):
-    """Combines the dataframes. Assumes that both dataframes have the same columns
-
-    Args:
-        first_dataframe (df): The first dataframe to be combined.
-        second_dataframe (df): The second dataframe to be combined.
-
-    Returns:
-        combined_dataframe (df): The combined dataframe.
-
-    """
-    combined_dataframe = pd.concat([first_dataframe, second_dataframe])
-    return combined_dataframe
-
-
-def invert_and_combine(original_dataframe):
-    """Inverts and combines the dataframes. Assumes that both dataframes have the same columns
-
-    Args:
-        original_dataframe (df): The dataframe to be inverted and combined with the original.
-
-    Returns:
-        new_dataframe (df): The combined dataframe.
-
-    """
-    inverted_dataframe = invert_dataframe(original_dataframe)
-    new_dataframe = combine_dataframe(original_dataframe, inverted_dataframe)
-    return new_dataframe
-
 
 X = invert_and_combine(X)
 y = invert_and_combine(y)
